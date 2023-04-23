8131e51ccba690ddacef51de4b5b21cabddbc7bc
Dr-Artem <artic12101997dr@gmail.com>
2023-04-23 13:32:12 +0300

    reverted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
-import ListOfPatients from './ListOfPatients/ListOfPatients';
 export const App = () => {
-    return <ListOfPatients />;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
