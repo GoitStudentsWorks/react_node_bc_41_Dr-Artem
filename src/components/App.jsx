d3a607ebc94407085cfcbaeefefaf2aa68d4ed94
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-04-24 11:28:14 +0300

    header html css arrows question

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import Header from './Header/Header';
+import MainPage from 'pages/MainPage/MainPage';
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    return (
+        <div
+        // style={{
+        //     height: '100vh',
+        //     display: 'flex',
+        //     justifyContent: 'center',
+        //     alignItems: 'center',
+        //     fontSize: 40,
+        //     color: '#010101',
+        // }}
+        >
+            <Header />
+            <MainPage />
+        </div>
+    );
 };
