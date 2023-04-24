9138e215b567b5c560021676327658e0a13f731d
Dr-Artem <artic12101997dr@gmail.com>
2023-04-25 01:51:57 +0300

    mui theme

--- src/muiTheme/theme.js
+++ src/muiTheme/theme.js
@@ -1,9 +1,23 @@
 import { createTheme } from '@mui/material/styles';
 
+const COLOR = {
+    green: '#477577',
+    darkGreen: '#0f3937',
+    lightGreen: '#68c494',
+    linearGreen: 'linear-gradient(253.01deg, #68c494 -56.38%, #477577 105.61%, #31496d 274.54%)',
+    orange: '#f3672b',
+    lightOrange: '#fc7b43',
+    white: '#FFFFFF',
+    gray: '#484f5c',
+    lightGray: '#fafafa',
+    black: '#111111',
+    lightBlue: '#edf5f5',
+};
+
 const theme = createTheme({
     breakpoints: {
         values: {
-            xs: 375,
+            sm: 375,
             md: 768,
             lg: 1440,
         },
@@ -48,11 +62,103 @@ const theme = createTheme({
             fontSize: '14px',
             lineHeight: 1.428,
         },
-        // button: {
-        //     fontWeight: 600,
-        //     fontSize: '14px',
-        //     lineHeight: 1.428,
-        // },
+        button: {
+            fontWeight: 600,
+            fontSize: '14px',
+            lineHeight: 1.428,
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: ({ ownerState }) => ({
+                    ...(ownerState.variant === 'contained' &&
+                        ownerState.color === 'secondary' && {
+                            backgroundColor: COLOR.lightOrange,
+                            color: COLOR.white,
+                            fontWeight: 600,
+                            fontSize: 12,
+                            lineHeight: 1.16,
+                            textTransform: 'uppercase',
+                            padding: '13px 32px',
+                            borderRadius: '12px',
+                            textAlign: 'center',
+
+                            '&:hover': {
+                                backgroundColor: COLOR.orange,
+                            },
+                            '&:focus': {
+                                backgroundColor: COLOR.orange,
+                                boxShadow: '0px 4px 8px rgba(17, 17, 17, 0.15)',
+                            },
+                        }),
+                    ...(ownerState.variant === 'outlined' &&
+                        ownerState.color === 'primary' && {
+                            border: '1px solid rgba(71, 117, 119, 0.3)',
+                            color: COLOR.green,
+                            fontWeight: 600,
+                            fontSize: 12,
+                            lineHeight: 1.16,
+                            textTransform: 'uppercase',
+                            padding: '16px 32px',
+                            borderRadius: '12px',
+                            textAlign: 'center',
+
+                            '&:hover': {
+                                backgroundColor: COLOR.darkGreen,
+                            },
+                            '&:focus': {
+                                backgroundColor: COLOR.darkGreen,
+                                boxShadow: '0px 4px 8px rgba(17, 17, 17, 0.15)',
+                            },
+                        }),
+                }),
+            },
+        },
+        MuiFormControl: {
+            styleOverrides: {
+                root: ({ ownerState }) => ({
+                    ...(ownerState.variant === 'standard' &&
+                        ownerState.color === 'primary' && {
+                            fontSize: '14px',
+                            lineHeight: 1.16,
+                        }),
+                }),
+            },
+        },
+        MuiInputLabel: {
+            styleOverrides: {
+                root: ({ ownerState }) => ({
+                    ...(ownerState.variant === 'standard' &&
+                        ownerState.color === 'primary' && {
+                            fontSize: 12,
+                            lineHeight: 1.16,
+                            fontWeight: 700,
+                            marginBottom: '8px',
+                            color: COLOR.black,
+                        }),
+                }),
+            },
+        },
+        MuiTextField: {
+            styleOverrides: {
+                root: ({ ownerState }) => ({
+                    ...(ownerState.variant === 'standard' &&
+                        ownerState.color === 'primary' && {
+                            fontSize: 14,
+                            lineHeight: 1.28,
+                            fontWeight: 400,
+                            borderRadius: '8px',
+                            border: `1px solid rgba(17, 17, 17, 0.1)`,
+                            padding: '14px 18px',
+
+                            '&:focus': {
+                                border: `1px solid ${COLOR.black}`,
+                            },
+                        }),
+                }),
+            },
+        },
     },
     spacing: 8,
 });
