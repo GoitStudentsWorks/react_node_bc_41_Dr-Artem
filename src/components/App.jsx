aae8fbc5ba1861c4a9567b0e9e4ff4ce66145d3e
Dr-Artem <artic12101997dr@gmail.com>
2023-04-24 20:58:01 +0300

    auth styles patch and routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,32 @@
-import Header from './Header/Header';
+import AuthPage from 'pages/Auth/AuthPage';
+import { Error } from 'pages/Error/Error';
+import { Layout } from 'pages/Layout/Layout';
 import MainPage from 'pages/MainPage/MainPage';
+// eslint-disable-next-line
+import { testPage } from 'pages/testPage/testPage';
+import { Route, Routes } from 'react-router';
+
 export const App = () => {
-    return <></>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<MainPage />} />
+                    <Route path="patient" element={<testPage />}>
+                        <Route path="history" element={<testPage />} />
+                        <Route path="doctors" element={<testPage />} />
+                        <Route path="visits-history" element={<testPage />} />
+                    </Route>
+                    <Route path="doctor" element={<testPage />}>
+                        <Route path="pesronal/:id" element={<testPage />} />
+                        <Route path="visits-history" element={<testPage />} />
+                        <Route path="patients-list" element={<testPage />} />
+                        <Route path="colleuges" element={<testPage />} />
+                    </Route>
+                </Route>
+                <Route path="auth/:typeAuth" element={<AuthPage />} />
+                <Route path="*" element={<Error />}></Route>
+            </Routes>
+        </>
+    );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import { App } from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router basename="/MedDoc">
+            <App />
+        </Router>
+    </React.StrictMode>
 );
