25b35d78278f4a4aba65e3a6ee4692aee01ebaec
yanarepak <112949438+yanarepak@users.noreply.github.com>
2023-04-25 12:39:02 +0300

    added yup validation

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,23 +1,53 @@
+import { useState } from 'react';
 import ShowPassword from 'components/ShowPassword/ShowPassword';
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 import css from '..//Login/LoginForm.module.css';
 
+// eslint-disable-next-line
+const regex = '^\+?3?8?(0\d{9})$';
+const passwordRules = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
+
+const schema = yup.object().shape({
+    phone: yup.string().matches(regex, 'Phone number is not valid').required('Phone is a required field'),
+    password: yup
+        .string()
+        .min(6, 'Password must be at least 6 characters')
+        .matches(passwordRules, 'Password must contain at least 8 characters, one uppercase, one number and one special case character'
+        )
+        .required('Password is a required field'),
+});
+
 export const LoginForm = () => {
+    const [showPassword, setShow] = useState(false);
+    // const [disabled, setDisabled] = useState(false);
+
+    const handleClick = () => {
+        setShow(show => !show);
+    };
+    
+    const initialValues = { phone: '', password: '' };
+
+    const handleSubmit = (values, { resetForm }) => {
+        // if(values === ''){
+        //     setDisabled(true);
+        //     alert('disabled');
+        // }
+        console.log(values);
+        resetForm();
+    };
+
     return (
         <div className={css.wrap}>
-            <Formik>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={schema}>
                 <Form>
                     <ul className={css.formWraper}>
                         <li className={css.formItem}>
                             <label htmlFor="phone" className={css.label}>
                                 Phone Number
                             </label>
-                            <Field
-                                type="tel"
-                                // name="phone"
-                                placeholder="+380 (_)__-__-__"
-                                className={css.input}
-                            />
+                            <Field type="tel" name="phone" placeholder="+380 (_)__-__-__" className={css.input} />
+                            <ErrorMessage name="phone" component="span" />
                         </li>
                         <li className={css.formItem}>
                             <label htmlFor="password" className={css.label}>
@@ -25,16 +55,18 @@ export const LoginForm = () => {
                             </label>
                             <div className={css.showWraper}>
                                 <Field
-                                    type="password"
-                                    // name="password"
+                                    type={showPassword ? 'true' : 'password'}
+                                    name="password"
                                     placeholder="Enter your password"
                                     className={css.input}
                                 />
-                                <ShowPassword />
+                                <ShowPassword isShown={showPassword} handleClick={handleClick} />
                             </div>
+                            <ErrorMessage name="password" component="span" />
                         </li>
                     </ul>
                     <button type="submit" className={css.formBtn}>
+                        {/* disabled={disabled}  */}
                         Log In
                     </button>
                 </Form>
--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -1,58 +1,87 @@
+import { useState } from 'react';
 import ShowPassword from 'components/ShowPassword/ShowPassword';
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 import css from '..//Register/Register.module.css';
 
+// eslint-disable-next-line
+const regex = '^\+\d{1,3}\s?\(\d{1,}\)\s?\d{1,}\s?\d{1,}\s?\d{1,}$';
+const passwordRules = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
+
+const schema = yup.object().shape({
+    username: yup
+        .string()
+        .min(3, 'Username must be at least 3 characters')
+        .max(200, 'Username must be less than or equal to 200 characters')
+        .matches(/^[aA-zZ\s]+$/, 'Only alphabets are allowed for this field ')
+        .required('Username is a required field'),
+    phone: yup.string().matches(regex, 'Phone number is not valid').required('Phone is a required field'),
+    password: yup
+        .string()
+        .min(6, 'Password must be at least 6 characters')
+        .matches(passwordRules, 'Password must contain at least 8 characters, one uppercase, one number and one special case character'
+        )
+        .required('Password is a required field'),
+});
+
 export const RegisterForm = () => {
-    // const initialValues = { username: '', email: '', password: '', role: '' };
+    const [showPassword, setShow] = useState(false);
+    const handleClick = () => {
+        setShow(show => !show);
+    };
+    
+    const initialValues = { username: '', phone: '', password: '', role: '' };
+
+    const handleSubmit = (values, { resetForm }) => {
+        console.log(values);
+        resetForm();
+    };
+
     return (
         <div className={css.wrap}>
-            <Formik>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} 
+            validationSchema={schema}
+            >
                 <Form>
                     <ul className={css.formWraper}>
                         <li className={css.formItem}>
                             <label htmlFor="username" className={css.label}>
                                 Name
                             </label>
-                            {/* дoдати name і value до інпутів */}
-                            <Field type="text" placeholder="Enter your name " className={css.input} />
+                            <Field type="text" name="username" placeholder="Enter your name " className={css.input} />
+                            <ErrorMessage name="username" component="span" />
                         </li>
                         <li className={css.formItem}>
                             <label htmlFor="phone" className={css.label}>
                                 Phone Number
                             </label>
                             <Field type="tel" name="phone" placeholder="+380 (__)___-___-__" className={css.input} />
+                            <ErrorMessage name="phone" component="span" />
                         </li>
                         <li className={css.formItem}>
                             <label htmlFor="password" className={css.label}>
                                 Password
                             </label>
                             <div className={css.showWraper}>
-                                <Field type="password" placeholder="Enter your password" className={css.input} />
-                                <ShowPassword />
+                                <ShowPassword handleClick={handleClick} isShown={showPassword} />
+                                <Field
+                                    name="password"
+                                    type={showPassword ? 'true' : 'password'}
+                                    placeholder="Enter your password"
+                                    className={css.input}
+                                />
                             </div>
+                            <ErrorMessage name="password" component="span" />
                         </li>
                         <li className={css.formItem}>
                             {/* RADIO */}
                             <div className={css.radio}>
-                                <Field
-                                    id="patient"
-                                    // name="role"
-                                    // value="patient"
-                                    type="radio"
-                                    className={css.radioInput}
-                                    checked
-                                />
+                                <Field name="role" value="patient" type="radio" className={css.radioInput} checked />
                                 <label htmlFor="patient" className={css.radioLabel}>
                                     Patient
                                 </label>
 
-                                <Field
-                                    id="doctor"
-                                    // name="role"
-                                    // value="doctor"
-                                    type="radio"
-                                    className={css.radioInput}
-                                />
+                                <Field name="role" value="doctor" type="radio" className={css.radioInput} />
                                 <label htmlFor="doctor" className={css.radioLabel}>
                                     Doctor
                                 </label>
