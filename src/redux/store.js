227bf02d6de139c3cae817187a25403545ef2f59
SunAndrea <andriishtohryn@gmail.com>
2023-05-01 21:27:43 +0300

    init redux/info

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/slice';
-
+import { infoReducer } from './info/slice';
 const authPersistConfig = {
     key: 'auth',
     storage,
@@ -12,6 +12,7 @@ const authPersistConfig = {
 export const store = configureStore({
     reducer: {
         auth: persistReducer(authPersistConfig, authReducer),
+        info: infoReducer,
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware({
