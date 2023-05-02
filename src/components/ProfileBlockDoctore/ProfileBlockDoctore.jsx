1a456b24c56ef701dfe120e98e3f17da6688d5f7
Dr-Artem <artic12101997dr@gmail.com>
2023-05-02 15:49:37 +0300

    modal reworked

--- src/components/ProfileBlockDoctore/ProfileBlockDoctore.jsx
+++ src/components/ProfileBlockDoctore/ProfileBlockDoctore.jsx
@@ -2,12 +2,12 @@ import { UilPen } from '@iconscout/react-unicons';
 import { Button, Typography } from '@mui/material';
 import { Badge } from 'components/Badge/Badge';
 import Card from 'components/Card/Card';
+import EditDoctorProfileModal from 'components/EditDoctorProfileModal/EditDoctorProdileModule';
 import ProfileImage from 'components/ProfileImage/ProfileImage';
 import StarRating from 'components/StarRating/StarRating';
-import { useLocation } from 'react-router';
 import { useState } from 'react';
+import { useLocation } from 'react-router';
 import css from './ProfileBlockDoctore.module.css';
-import EditDoctorProfileModal from 'components/EditDoctorProfileModal/EditDoctorProdileModule';
 
 const ident = 1;
 
@@ -31,27 +31,6 @@ export const ProfileBlockDoctore = ({ children }) => {
                             </li>
                         )}
 
-<<<<<<< HEAD
-                    <li className={css.item}>
-                        Date of birth:<p className={css.data}>12/05/1992</p>
-                    </li>
-                    <li className={css.item}>
-                        Phone number:<p className={css.data}>+380 (97) 77 77 7 77</p>
-                    </li>
-                    <li className={css.item}>
-                        Price:<p className={css.data}>750 UAH /1 hour</p>
-                    </li>
-                </ul>
-                {/* {personalLoc && ( */}
-                    <Button color="primary" onClick={() => setAppModal(!appModal)} >
-                        <UilPen style={{ width: '20px', height: '20px' }} />
-                        <Typography color="text.black" sx={{ lineHeight: 1.285, textTransform: 'none' }}>
-                            Edit profile
-                        </Typography>
-                    </Button>
-                {/* )} */}
-                {children}
-=======
                         <li className={css.item}>
                             Date of birth:<p className={css.data}>12/05/1992</p>
                         </li>
@@ -62,19 +41,18 @@ export const ProfileBlockDoctore = ({ children }) => {
                             Price:<p className={css.data}>750 UAH /1 hour</p>
                         </li>
                     </ul>
-                    {personalLoc && (
-                        <Button color="primary">
-                            <UilPen style={{ width: '20px', height: '20px' }} />
-                            <Typography color="text.black" sx={{ lineHeight: 1.285, textTransform: 'none' }}>
-                                Edit profile
-                            </Typography>
-                        </Button>
-                    )}
-                    {children}
                 </div>
->>>>>>> baf27c8c0d2eb49929452ca12851d9686528dbe2
+                {/* {personalLoc && ( */}
+                <Button color="primary" onClick={() => setAppModal(!appModal)}>
+                    <UilPen style={{ width: '20px', height: '20px' }} />
+                    <Typography color="text.black" sx={{ lineHeight: 1.285, textTransform: 'none' }}>
+                        Edit profile
+                    </Typography>
+                </Button>
+                {/* )} */}
+                {children}
             </div>
-        <EditDoctorProfileModal open={appModal} setApp={setAppModal}/>
+            <EditDoctorProfileModal open={appModal} setApp={setAppModal} />
         </Card>
     );
 };
