5aad377e172cc0a69da55415312a166a0d20f09f
SunAndrea <andriishtohryn@gmail.com>
2023-05-02 14:10:47 +0300

    update redux

--- src/redux/info/operation.js
+++ src/redux/info/operation.js
@@ -16,6 +16,63 @@ export const getUserInfoById = createAsyncThunk('/getUserInfoById', async (id, {
         const { data } = await axios.get(`/info/${id}`);
         return data;
     } catch (error) {
-        return rejectWithValue(error.massage);
+        return rejectWithValue(error.message);
+    }
+});
+
+export const getAllUsersForRole = createAsyncThunk('/getAllUsersForRole', async (role, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.get(`/info/all/${role}`);
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+});
+
+export const updateUserInfo = createAsyncThunk('/updateUserInfo', async (information, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.patch('/info/update');
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+});
+
+export const updateUserRating = createAsyncThunk('/updateUserRating', async (id, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.patch(`/info/update/rating/${id}`);
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+});
+
+export const addUserExperience = createAsyncThunk('/addUserExperience', async (information, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post('/info/experience');
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+});
+
+export const updateUserExperience = createAsyncThunk(
+    '/updateUserExperience',
+    async (id, updatedExperience, { rejectWithValue }) => {
+        try {
+            const { data } = await axios.patch(`/info/experience/${id}`);
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const deleteUserExperience = createAsyncThunk('/deleteUserExperience', async (id, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.delete(`/info/experience/${id}`);
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
     }
 });
